4e942b208b2bd0ad89ad1818b2f5c714e50835e1
TangChangXin <592609672@qq.com>
2022-05-02 15:20:32 +0800

    模型定义

--- model.py
+++ model.py
@@ -19,12 +19,43 @@ transformer编码器的输入似乎是768，所以
 '''
 
 
+
 """
 MAE随机选择图像块应该在MAE类中实现
 """
-class 多头自己注意力(nn.Module):
-    def __init__(self):
-        super(多头自己注意力, self).__init__()
+
+
+# 原论文中在多头注意力之后 使用的是dropout层，有其他大神用droppath实现，效果更好
+def drop_path(x, drop_prob: float = 0., training: bool = False):
+    """
+    Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
+    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
+    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
+    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
+    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
+    'survival rate' as the argument.
+    """
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
+    random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+    random_tensor.floor_()  # binarize
+    output = x.div(keep_prob) * random_tensor
+    return output
+
+
+class DropPath(nn.Module):
+    """
+    Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
+    """
+    def __init__(self, drop_prob=None):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training)
+
 
 
 class 图像块嵌入向量(nn.Module):
@@ -56,55 +87,125 @@ class 图像块嵌入向量(nn.Module):
         return x
 
 
-class 多头注意力(nn.Module):
+class 自注意力(nn.Module):
+    """
+    多头自注意力模块
+    """
     def __init__(self,
-                 嵌入向量的维度,
-                 注意力头的数量,
-                 注意力丢弃率=0.,
+                 嵌入向量维度,
+                 注意力头数量,
+                 qkv_偏差=False,
+                 缩小因子=None,
+                 自注意力丢弃率=0.,
                  全连接层丢弃率=0.):
-        super(多头注意力, self).__init__()
-        self.注意力头的数量 = 注意力头的数量
-        注意力头维度 = 嵌入向量的维度 // 注意力头的数量 # 每个注意力头的维度大小
-        self.缩小 = 注意力头维度 ** -0.5
+        super(自注意力, self).__init__()
+        self.注意力头数量 = 注意力头数量
+        注意力头维度 = 嵌入向量维度 // 注意力头数量  # 每个注意力头的维度大小
+        self.缩小因子 = 缩小因子 or 注意力头维度 ** -0.5
         # 通过一个全连接层生成qkv三个向量，有助于并行化计算
-        self.qkv = nn.Linear(嵌入向量的维度, 嵌入向量的维度 * 3, bias=False)
-        self.注意力丢弃 = nn.Dropout(注意力丢弃率)
+        # 小数据集中qkv直接false，但是原版用参数控制true
+        self.qkv = nn.Linear(嵌入向量维度, 嵌入向量维度 * 3, bias=qkv_偏差)
+        self.自注意力丢弃 = nn.Dropout(自注意力丢弃率) # 自注意力信息经过softmax后的Dropout
 
         # 多头注意力的输出拼接后与Wo相乘得到最终的输出。Wo矩阵通过全连接层实现
-        self.proj = nn.Linear(嵌入向量的维度, 嵌入向量的维度)
-        self.proj_drop = nn.Dropout(全连接层丢弃率)
+        self.线性投影 = nn.Linear(嵌入向量维度, 嵌入向量维度)
+        self.线性投影丢弃 = nn.Dropout(全连接层丢弃率)
 
-        def forward(self, x):
-            # [batch_size, num_patches + 1, embed_dim]
-            # 每一批图片数。图像块的数量加1是因为算上class token，我的方法按照纵向的深度计算           B, N, C = x.shape
-            print(x.shape)
-            B, N, C = x.shape
+    def forward(self, x):
+        print('\n')
+        print("输入数据的形状", x.shape)
+        批量大小, 图像块数量, 嵌入向量维度 = x.shape
+        # TODO 我不分类需要分类信息嵌入向量吗？
+        # [batch_size, num_patches + 1, embed_dim] 每一批图片数。图像块的数量加1是因为算上class token，我的方法按照纵向的深度计算
+        # qkv(): -> [批量大小, 图像块数量 + 1, 3 * 嵌入向量维度] 我不分类可能不加分类嵌入向量。
+        # 3对应qkv三个向量。后面两个维度的数据可以理解为将嵌入向量按照注意力头的数量平均划分之后分别送入不同的注意力头中计算
+        # reshape: -> [批量大小, 图像块数量 + 1, 3, 注意力头数量, 每个注意力头的嵌入向量维度]。
+        # permute: -> [3, 批量大小, 注意力头数量, 图像块数量 + 1, 每个注意力头的嵌入向量维度]
+        qkv = self.qkv(x).reshape(批量大小, 图像块数量, 3, self.注意力头数量, 嵌入向量维度 // self.注意力头数量).permute(2, 0, 3, 1, 4)
 
-            # qkv(): -> [batch_size, num_patches + 1, 3 * total_embed_dim]
-            # reshape: -> [batch_size, num_patches + 1, 3, num_heads, embed_dim_per_head]
-            # permute: -> [3, batch_size, num_heads, num_patches + 1, embed_dim_per_head]
+        print('qkv的形状', qkv.shape)
 
-            qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        # 分别取出qkv向量。向量的形状[批量大小, 注意力头数量, 图像块数量 + 1, 每个注意力头的嵌入向量维度]
+        q, k, v = qkv[0], qkv[1], qkv[2]
 
-            # [batch_size, num_heads, num_patches + 1, embed_dim_per_head]
-            q, k, v = qkv[0], qkv[1], qkv[2]  # make torchscript happy (cannot use tensor as tuple)
+        # k最后两个维度调换位置，k.transpose：-> [批量大小, 注意力头数量, 每个注意力头的嵌入向量维度, 图像块数量 + 1]
+        # q和k矩阵相乘: -> [批量大小, 注意力头数量, 图像块数量 + 1, 图像块数量 + 1]
+        自注意力信息 = (q @ k.transpose(-2, -1)) * self.缩小因子  # q和k进行多维矩阵乘法时，实际只有最后两个维度相乘
+        自注意力信息 = 自注意力信息.softmax(dim=-1)  # 沿着最后一个维度进行柔性最大值处理。
+        自注意力信息 = self.自注意力丢弃(自注意力信息)
 
-            # transpose: -> [batch_size, num_heads, embed_dim_per_head, num_patches + 1]
-            # @: multiply -> [batch_size, num_heads, num_patches + 1, num_patches + 1]
+        # 自注意力信息和v矩阵相乘：-> [批量大小, 注意力头数量, 图像块数量 + 1, 每个注意力头的嵌入向量维度]
+        # transpose: -> [批量大小, 图像块数量 + 1, 注意力头数量, 每个注意力头的嵌入向量维度]
+        # reshape: -> [批量大小, 图像块数量 + 1, 嵌入向量维度]
+        自注意力信息 = (自注意力信息 @ v).transpose(1, 2).reshape(批量大小, 图像块数量, 嵌入向量维度)  # 和v矩阵相乘 加权求和
+        自注意力信息 = self.线性投影(自注意力信息)
+        自注意力信息 = self.线性投影丢弃(自注意力信息)
+        return 自注意力信息
 
-            attn = (q @ k.transpose(-2, -1)) * self.scale  # q和k进行多维矩阵乘法时，实际只有最后两个维度相乘
-            attn = attn.softmax(dim=-1)  # 似乎是二维矩阵，按行进行柔性最大值处理。
-            attn = self.attn_drop(attn)
 
-            # @: multiply -> [batch_size, num_heads, num_patches + 1, embed_dim_per_head]
-            # transpose: -> [batch_size, num_patches + 1, num_heads, embed_dim_per_head]
-            # reshape: -> [batch_size, num_patches + 1, total_embed_dim]
-            x = (attn @ v).transpose(1, 2).reshape(B, N, C)  # 和v矩阵相乘 加权求和
-            x = self.proj(x)
-            x = self.proj_drop(x)
-            return x
+class 多层感知机(nn.Module):
+    """
+    transformer编码块中的MLP模块
+    """
+    def __init__(self, 输入大小, 隐藏层大小=None, 输出大小=None, 激活函数=nn.GELU, 丢弃率=0.):
+        super(多层感知机, self).__init__()
+        输出大小 = 输出大小 or 输入大小
+        隐藏层大小 = 隐藏层大小 or 输入大小
+        self.全连接1 = nn.Linear(输入大小, 隐藏层大小)
+        self.激活函数 = 激活函数()
+        self.全连接2 = nn.Linear(隐藏层大小, 输出大小)
+        self.随机丢弃 = nn.Dropout(丢弃率)
 
+    def forward(self, x):
+        x = self.全连接1(x)
+        x = self.激活函数(x)
+        x = self.随机丢弃(x)
+        x = self.全连接2(x)
+        x = self.随机丢弃(x)
+        return x
+
+
+class 编码块(nn.Module):
+    """
+    transformer编码器模块，将自注意力模块和MLP模块结合起来
+    """
+    def __init__(self,
+                 特征维度, # 输入向量的维度
+                 注意力头数量, # 注意力头的数量
+                 全连接层扩增率=4., # 第一个全连接层的节点是输入节点的四倍，对应768变为2304
+                 qkv_偏差=False, # 是否使用偏差
+                 缩小因子=None, # 缩放因子
+                 自注意力丢弃率=0.,  # qkv矩阵计算输出之后经过softmax层后的dropout
+                 全连接层丢弃率=0., # 对应多头注意力模块中最后一个全连接层
+                 drop_path_ratio=0., # 对应的编码块中droppath
+                 激活函数=nn.GELU, # 默认激活函数
+                 标准化=nn.LayerNorm): # 默认归一化方式
+        super(编码块, self).__init__()
+        self.标准化层1 = 标准化(特征维度) # 编码块中的第一个LN层
+        self.多头自注意力 = 自注意力(特征维度, 注意力头数量=注意力头数量, qkv_偏差=qkv_偏差, 缩小因子=缩小因子, 自注意力丢弃率=自注意力丢弃率, 全连接层丢弃率=全连接层丢弃率)
+        # TODO 如果drop_path_ratio大于0，就采用droppath否则直接用恒等映射。作者认为droppath比dropout效果好
+        self.drop_path = DropPath(drop_path_ratio) if drop_path_ratio > 0. else nn.Identity()
+        # 对应MLP块中最前面的的LN层
+        self.标准化层2 = 标准化(特征维度)
+        隐藏层维度 = int(特征维度 * 全连接层扩增率) # MLP模块中第一个全连接层对应的隐藏层节点数量，这里相当于维度增加。
+        self.多层感知机 = 多层感知机(输入大小=特征维度, 隐藏层大小=隐藏层维度, 激活函数=激活函数, 丢弃率=全连接层丢弃率)
+
+    def forward(self, x):
+        x = x + self.drop_path(self.多头自注意力(self.标准化层1(x)))
+        x = x + self.drop_path(self.多层感知机(self.标准化层2(x)))
+        return x
+# '''
 if __name__ == '__main__':
-    测试模型 = 图像块嵌入向量(1, 512, nn.LayerNorm)
+    # 测试模型 = 自注意力(512, 8, 0.2, 0.2)
+    # 测试模型.to(torch.device('cuda:0'))
+    # summary(测试模型, input_size=(2, 256, 16, 16), batch_size=1, device='cuda')
+
+    # 测试模型 = 自注意力(嵌入向量维度=512, 注意力头数量=8, 自注意力丢弃率=0.2, 全连接层丢弃率=0.2)
+    # 测试模型.to(torch.device('cuda:0'))
+    # summary(测试模型, input_size=(256,512), batch_size=1, device='cuda')
+
+    测试模型 = 编码块(特征维度=512, 注意力头数量=8, 自注意力丢弃率=0.2, 全连接层丢弃率=0.2, drop_path_ratio=0.2)
     测试模型.to(torch.device('cuda:0'))
-    summary(测试模型, input_size=(2, 256, 16, 16), batch_size=1, device='cuda')  #
+    summary(测试模型, input_size=(256,512), batch_size=7, device='cuda')
+
+# '''
