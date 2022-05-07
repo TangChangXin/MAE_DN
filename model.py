327910c7f4c48c1becac5b8331a45b061be695b7
TangChangXin <592609672@qq.com>
2022-05-07 15:52:44 +0800

    模型定义

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import numpy as np
 from functools import partial
 from collections import OrderedDict
 from torchsummary import summary
@@ -60,7 +61,7 @@ class DropPath(nn.Module):
         return drop_path(x, self.drop_prob, self.training)
 
 
-class 图像块嵌入(nn.Module):
+class 立体图像块嵌入(nn.Module):
     """
     如何将2维和3维整合在一起。3维输入的应该是
     现在认为输入的图像都是已经划分成图像块之后的结果。有监督训练的时候在另一个类里对有标签的图像划分。
@@ -75,7 +76,7 @@ class 图像块嵌入(nn.Module):
         :param 图像块嵌入向量的维度:
         """
         # todo 现在是3维为例
-        super(图像块嵌入, self).__init__()
+        super(立体图像块嵌入, self).__init__()
         self.图像形状 = 图像形状
         self.图像块的大小 = 16
         self.图像块数量 = 256 # TODO 注意修改
@@ -203,11 +204,10 @@ class 编码块(nn.Module):
 
 
 class 融合网络无cls(nn.Module):
-    def __init__(self, 图像大小=224, 图像块大小=16, 输入通道数=3, 嵌入向量维度=512, 深度=12, 注意力头数量=8, 多层感知机扩增率=4.0,
-                 qkv_偏差=True, qk_缩小因子=None, 全连接丢弃率=0., 自注意力丢弃率=0., drop_path_ratio=0., 图像嵌入层=图像块嵌入, 标准化=None,):
+    def __init__(self, 图像形状=304, 图像块大小=16, 输入通道数=3, 嵌入向量维度=512, 深度=12, 注意力头数量=8, 多层感知机扩增率=4.0,
+                 qkv_偏差=True, qk_缩小因子=None, 全连接丢弃率=0., 自注意力丢弃率=0., drop_path_ratio=0., 图像嵌入层=立体图像块嵌入, 标准化=None,):
         """
 
-        :param 图像大小:
         :param 图像块大小:
         :param 输入通道数: 卷积生成图像块时用到
         :param 嵌入向量维度: (int) 大小为图像块的宽度乘高度再乘2。也可以自己通过卷积输出通道数来指定。
@@ -225,7 +225,7 @@ class 融合网络无cls(nn.Module):
         super(融合网络无cls, self).__init__()
         self.num_features = 嵌入向量维度  # num_features for consistency with other models
         标准化 = 标准化 or partial(nn.LayerNorm, eps=1e-6)
-        self.图像块嵌入向量 = 图像嵌入层(图像形状=304, 图像块嵌入向量的维度=嵌入向量维度)
+        self.图像块嵌入向量 = 图像嵌入层(图像形状=图像形状, 图像块嵌入向量的维度=嵌入向量维度)
         图像块数量 = self.图像块嵌入向量.图像块数量
 
         # 第一个维度的1对应的是批量
@@ -262,7 +262,8 @@ class 融合网络无cls(nn.Module):
         x = self.激活函数(x)
         x = torch.transpose(x, 1, 2)
         x = self.特征降维(x) # 输出形状[1, 256， 1]
-        x = torch.reshape(x, (-1, 16, 16)) # 输出形状[批量, 16, 16]
+        # x = torch.reshape(x, (-1, 16, 16)) # 输出形状[批量, 16, 16]
+        x = torch.reshape(x, (16, 16))  # 应该输出形状[批量, 16, 16]
         # print(x.shape)
         return x
 
@@ -271,7 +272,7 @@ class 融合网络(nn.Module):
     def __init__(self, 图像大小=224, 图像块大小=16, 输入通道数=3, 分类数目=1000,
                  嵌入向量维度=512, 深度=12, 注意力头数量=8, 多层感知机扩增率=4.0, qkv_偏差=True,
                  qk_缩小因子=None, representation_size=None, 蒸馏=False, 全连接丢弃率=0.,
-                 自注意力丢弃率=0., drop_path_ratio=0., 图像嵌入层=图像块嵌入, 标准化=None,):
+                 自注意力丢弃率=0., drop_path_ratio=0., 图像嵌入层=立体图像块嵌入, 标准化=None,):
         """
 
         :param 图像大小:
@@ -405,11 +406,347 @@ class 自监督重建OCTA图像(nn.Module):
     """
     现在考虑传进来的数据是已经划分块之后的结果
     """
-    def __init__(self):
+    def __init__(self, 图像形状, 图像块嵌入向量的维度):
         super(自监督重建OCTA图像, self).__init__()
+        self.标准化 = nn.LayerNorm(图像块嵌入向量的维度)
+
+    def forward(self, x):
+        """
+        输出数据的形式[批量, 16, 16]
+        :param x:
+        :return:
+        """
 
 
 
+
+
+# --------------------------------------------------------
+# 2D sine-cosine position embedding
+# References:
+# Transformer: https://github.com/tensorflow/models/blob/master/official/nlp/transformer/model_utils.py
+# MoCo v3: https://github.com/facebookresearch/moco-v3
+# --------------------------------------------------------
+
+def get_2d_sincos_pos_embed_from_grid(embed_dim, grid):
+    assert embed_dim % 2 == 0
+
+    # use half of dimensions to encode grid_h
+    emb_h = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[0])  # (H*W, D/2)
+    emb_w = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[1])  # (H*W, D/2)
+
+    emb = np.concatenate([emb_h, emb_w], axis=1) # (H*W, D)
+    return emb
+
+
+def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
+    """
+    embed_dim: output dimension for each position
+    pos: a list of positions to be encoded: size (M,)
+    out: (M, D)
+    """
+    assert embed_dim % 2 == 0
+    omega = np.arange(embed_dim // 2, dtype=np.float)
+    omega /= embed_dim / 2.
+    omega = 1. / 10000**omega  # (D/2,)
+
+    pos = pos.reshape(-1)  # (M,)
+    out = np.einsum('m,d->md', pos, omega)  # (M, D/2), outer product
+
+    emb_sin = np.sin(out) # (M, D/2)
+    emb_cos = np.cos(out) # (M, D/2)
+
+    emb = np.concatenate([emb_sin, emb_cos], axis=1)  # (M, D)
+    return emb
+
+
+def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
+    """
+    grid_size: int of the grid height and width
+    return:
+    pos_embed: [grid_size*grid_size, embed_dim] or [1+grid_size*grid_size, embed_dim] (w/ or w/o cls_token)
+    """
+    grid_h = np.arange(grid_size, dtype=np.float32)
+    grid_w = np.arange(grid_size, dtype=np.float32)
+    grid = np.meshgrid(grid_w, grid_h)  # here w goes first
+    grid = np.stack(grid, axis=0)
+
+    grid = grid.reshape([2, 1, grid_size, grid_size])
+    pos_embed = get_2d_sincos_pos_embed_from_grid(embed_dim, grid)
+    if cls_token:
+        pos_embed = np.concatenate([np.zeros([1, embed_dim]), pos_embed], axis=0)
+    return pos_embed
+
+
+from timm.models.vision_transformer import PatchEmbed, Block
+
+
+class MaskedAutoencoderViT(nn.Module):
+    """ Masked Autoencoder with VisionTransformer backbone
+    """
+
+    def __init__(self, img_size=224, patch_size=16, in_chans=3,
+                 embed_dim=1024, depth=24, num_heads=16,
+                 decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+                 mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False):
+        super().__init__()
+
+        # --------------------------------------------------------------------------
+        # MAE encoder specifics
+        self.patch_embed = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
+        num_patches = self.patch_embed.num_patches
+
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim),
+                                      requires_grad=False)  # fixed sin-cos embedding
+
+        self.blocks = nn.ModuleList([
+            Block(embed_dim, num_heads, mlp_ratio, qkv_bias=True, qk_scale=None, norm_layer=norm_layer)
+            for i in range(depth)])
+        self.norm = norm_layer(embed_dim)
+        # --------------------------------------------------------------------------
+
+        # --------------------------------------------------------------------------
+        # MAE decoder specifics
+        self.decoder_embed = nn.Linear(embed_dim, decoder_embed_dim, bias=True)
+
+        # 替换被遮掩的图像块
+        self.mask_token = nn.Parameter(torch.zeros(1, 1, decoder_embed_dim))
+
+        # 加1是因为解码也需要cls_token
+        self.decoder_pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, decoder_embed_dim),
+                                              requires_grad=False)  # fixed sin-cos embedding
+
+        self.decoder_blocks = nn.ModuleList([
+            Block(decoder_embed_dim, decoder_num_heads, mlp_ratio, qkv_bias=True, qk_scale=None, norm_layer=norm_layer)
+            for i in range(decoder_depth)])
+
+        self.decoder_norm = norm_layer(decoder_embed_dim)
+        self.decoder_pred = nn.Linear(decoder_embed_dim, patch_size ** 2 * in_chans, bias=True)  # decoder to patch
+        # --------------------------------------------------------------------------
+
+        self.norm_pix_loss = norm_pix_loss
+
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        # initialization
+        # initialize (and freeze) pos_embed by sin-cos embedding
+        # 编码器的位置嵌入
+        pos_embed = get_2d_sincos_pos_embed(self.pos_embed.shape[-1], int(self.patch_embed.num_patches ** .5),
+                                            cls_token=True)
+        self.pos_embed.data.copy_(torch.from_numpy(pos_embed).float().unsqueeze(0))
+
+        decoder_pos_embed = get_2d_sincos_pos_embed(self.decoder_pos_embed.shape[-1],
+                                                    int(self.patch_embed.num_patches ** .5), cls_token=True)
+        self.decoder_pos_embed.data.copy_(torch.from_numpy(decoder_pos_embed).float().unsqueeze(0))
+
+        # initialize patch_embed like nn.Linear (instead of nn.Conv2d)
+        w = self.patch_embed.proj.weight.data
+        torch.nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+
+        # timm's trunc_normal_(std=.02) is effectively normal_(std=0.02) as cutoff is too big (2.)
+        torch.nn.init.normal_(self.cls_token, std=.02)
+        torch.nn.init.normal_(self.mask_token, std=.02)
+
+        # initialize nn.Linear and nn.LayerNorm
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            torch.nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    # 将图片划分成块
+    def patchify(self, imgs):
+        """
+        imgs: (N, 3, H, W)
+        x: (N, L, patch_size**2 *3)
+        """
+        p = self.patch_embed.patch_size[0]
+        assert imgs.shape[2] == imgs.shape[3] and imgs.shape[2] % p == 0
+
+        h = w = imgs.shape[2] // p
+        x = imgs.reshape(shape=(imgs.shape[0], 3, h, p, w, p))
+        x = torch.einsum('nchpwq->nhwpqc', x)
+        x = x.reshape(shape=(imgs.shape[0], h * w, p ** 2 * 3))
+        return x
+
+    #
+    def unpatchify(self, x):
+        """
+        x: (N, L, patch_size**2 *3)
+        imgs: (N, 3, H, W)
+        """
+        p = self.patch_embed.patch_size[0]
+        h = w = int(x.shape[1] ** .5)
+        assert h * w == x.shape[1]
+
+        x = x.reshape(shape=(x.shape[0], h, w, p, p, 3))
+        x = torch.einsum('nhwpqc->nchpwq', x)
+        imgs = x.reshape(shape=(x.shape[0], 3, h * p, h * p))
+        return imgs
+
+    # TODO 随机掩码函数，很重要
+    def random_masking(self, x, mask_ratio):
+        """
+        Perform per-sample random masking by per-sample shuffling.
+        Per-sample shuffling is done by argsort random noise.
+        x: [N, L, D], sequence
+        """
+        N, L, D = x.shape  # batch, length, dim
+        len_keep = int(L * (1 - mask_ratio))
+
+        # 二维随机噪声矩阵
+        noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
+
+        # sort noise for each sample。argsort()返回的是元素对应的索引
+        # ids_shuffle是用来选择哪些元素做掩码
+        ids_shuffle = torch.argsort(noise, dim=1)  # 升序排列ascend: small is keep, large is remove
+        # ids_restore是用于在编码之后对图像块的顺序进行还原并输入解码器？
+        ids_restore = torch.argsort(ids_shuffle, dim=1) # 对上一步得到的索引排序
+
+        # keep the first subset
+        ids_keep = ids_shuffle[:, :len_keep]
+        #
+        x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
+
+        # generate the binary mask: 0 is keep, 1 is remove
+        #
+        mask = torch.ones([N, L], device=x.device)
+        mask[:, :len_keep] = 0
+        # unshuffle to get the binary mask
+        mask = torch.gather(mask, dim=1, index=ids_restore)
+        return x_masked, mask, ids_restore
+
+    def forward_encoder(self, x, mask_ratio):
+        # embed patches
+        x = self.patch_embed(x)
+
+        # add pos embed w/o cls token
+        x = x + self.pos_embed[:, 1:, :]
+
+        # masking: length -> length * mask_ratio
+        x, mask, ids_restore = self.random_masking(x, mask_ratio)
+
+        # append cls token
+        cls_token = self.cls_token + self.pos_embed[:, :1, :]
+        cls_tokens = cls_token.expand(x.shape[0], -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+
+        # apply Transformer blocks
+        for blk in self.blocks:
+            x = blk(x)
+        x = self.norm(x)
+
+        return x, mask, ids_restore
+
+    def forward_decoder(self, x, ids_restore):
+        # embed tokens
+        x = self.decoder_embed(x)
+
+        # append mask tokens to sequence
+        mask_tokens = self.mask_token.repeat(x.shape[0], ids_restore.shape[1] + 1 - x.shape[1], 1)
+        x_ = torch.cat([x[:, 1:, :], mask_tokens], dim=1)  # no cls token
+        x_ = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))  # unshuffle
+        x = torch.cat([x[:, :1, :], x_], dim=1)  # append cls token
+
+        # add pos embed
+        x = x + self.decoder_pos_embed
+
+        # apply Transformer blocks
+        for blk in self.decoder_blocks:
+            x = blk(x)
+        x = self.decoder_norm(x)
+
+        # predictor projection
+        x = self.decoder_pred(x)
+
+        # remove cls token
+        x = x[:, 1:, :]
+
+        return x
+
+    def forward_loss(self, imgs, pred, mask):
+        """
+        imgs: [N, 3, H, W]
+        pred: [N, L, p*p*3]
+        mask: [N, L], 0 is keep, 1 is remove,
+        """
+        target = self.patchify(imgs)
+        if self.norm_pix_loss:
+            mean = target.mean(dim=-1, keepdim=True)
+            var = target.var(dim=-1, keepdim=True)
+            target = (target - mean) / (var + 1.e-6) ** .5
+
+        loss = (pred - target) ** 2
+        loss = loss.mean(dim=-1)  # [N, L], mean loss per patch
+
+        loss = (loss * mask).sum() / mask.sum()  # mean loss on removed patches
+        return loss
+
+    def forward(self, imgs, mask_ratio=0.75):
+        latent, mask, ids_restore = self.forward_encoder(imgs, mask_ratio)
+        pred = self.forward_decoder(latent, ids_restore)  # [N, L, p*p*3]
+        loss = self.forward_loss(imgs, pred, mask)
+        return loss, pred, mask
+
+
+def mae_vit_base_patch16_dec512d8b(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=16, embed_dim=768, depth=12, num_heads=12,
+        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+# set recommended archs
+mae_vit_base_patch16 = mae_vit_base_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
+
+
+
+
+
+
+
+class 平面图像块嵌入(nn.Module):
+    """
+    如何将2维和3维整合在一起。3维输入的应该是
+    现在认为输入的图像都是已经划分成图像块之后的结果。有监督训练的时候在另一个类里对有标签的图像划分。
+    """
+    def __init__(self, 图像形状, 图像块嵌入向量的维度):
+        r"""
+        加if分支处理3维和2维的区别
+        输入通道数暂时没用到，有监督训练时如何对输入的图像划分
+        3维输入数据形状BCDHW 1*2*256*16*16，在256中随机选取部分用来训练，输出1*256的向量，然后整形为1*16*16。
+
+        :param 图像形状:
+        :param 图像块嵌入向量的维度:
+        """
+        # todo 现在是3维为例
+        super(平面图像块嵌入, self).__init__()
+        self.图像形状 = 图像形状
+        self.图像块的大小 = 16
+        self.图像块数量 = 256 # TODO 注意修改
+        self.标准化 = nn.LayerNorm(图像块嵌入向量的维度)
+        self.图像分块 = nn.Unfold(kernel_size=16, stride=16)
+
+    def forward(self, x):
+        """
+        # 分块函数 F.unfold
+        # 通过立体图像输入应该是[256*16*16]，但我真实的图片应该是[批量，宽度，高度，通道数]
+        # TODO 现在只考虑从立体数据融合来的。输入256*16*16，展平后256*256。
+        x = torch.flatten(x, start_dim=1)
+        # 再展平后输出1*256*512，形状[B, D, HWC]
+        x = torch.flatten(x, 2)
+        x = self.标准化(x)
+        """
+        x = self.图像分块(x)
+        return x
 
 
 def 权重初始化(模块):
@@ -431,7 +768,7 @@ def 权重初始化(模块):
 
 # '''
 if __name__ == '__main__':
-    # 测试模型 = 图像块嵌入(304, 512)
+    # 测试模型 = 立体图像块嵌入(304, 512)
     # 测试模型.to(torch.device('cuda:0'))
     # print('\n')
     # summary(测试模型, input_size=(2, 256, 16, 16), batch_size=3, device='cuda') # [3, 256, 512]
@@ -456,13 +793,20 @@ if __name__ == '__main__':
     # print('\n')
     # summary(测试模型, input_size=(2, 256, 16, 16), batch_size=3, device='cuda')
 
-    测试模型 = 融合网络无cls() # 输出[3, 256, 512]
-    测试模型.to(torch.device('cuda:0'))
-    print('\n')
-    summary(测试模型, input_size=(2, 256, 16, 16), batch_size=1, device='cuda')
+    # 测试模型 = 融合网络无cls() # 输出[3, 256, 512]
+    # 测试模型.to(torch.device('cuda:0'))
+    # print('\n')
+    # summary(测试模型, input_size=(2, 256, 16, 16), batch_size=1, device='cuda')
 
     # print(测试模型)
 
     # with SummaryWriter(comment='测试模型') as 可视化:
     #     可视化.add_graph(测试模型, torch.randn(1, 2, 256, 16, 16))
+
+
+    测试模型 = 平面图像块嵌入(304, 512) # 输出[3, 256, 512]
+    测试模型.to(torch.device('cuda:0'))
+    print('\n')
+    summary(测试模型, input_size=(1, 304, 304), batch_size=1, device='cuda')
+
 # '''
